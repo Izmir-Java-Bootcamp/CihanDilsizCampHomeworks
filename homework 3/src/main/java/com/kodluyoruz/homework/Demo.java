495ccd71a34fd88f95bcc4734cda7277bcd03b99
Cihan Dilsiz <74474016+cdilsiz5@users.noreply.github.com>
2021-10-18 19:48:26 +0300

    Update Demo.java

--- homework 3/src/main/java/com/kodluyoruz/homework/Demo.java
+++ homework 3/src/main/java/com/kodluyoruz/homework/Demo.java
@@ -1,6 +1,6 @@
 package com.kodluyoruz.homework;
 
-
+// Main class will be added
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
