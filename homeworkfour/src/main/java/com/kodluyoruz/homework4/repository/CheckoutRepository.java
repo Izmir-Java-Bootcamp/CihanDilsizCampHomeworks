d76f578e51417682fe73f57feed0f857fa7557e3
Cihan Dilsiz <74474016+cdilsiz5@users.noreply.github.com>
2023-06-05 05:04:02 -0700

    Delete homeworkfour directory

--- homeworkfour/src/main/java/com/kodluyoruz/homework4/repository/CheckoutRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kodluyoruz.homework4.repository;
-
-import com.kodluyoruz.homework4.model.entity.Checkout;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CheckoutRepository extends JpaRepository<Checkout,Integer> {
-}
--- homeworkfour/src/main/java/com/kodluyoruz/homework4/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kodluyoruz.homework4.repository;
-
-import com.kodluyoruz.homework4.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
